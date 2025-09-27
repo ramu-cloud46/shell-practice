55a70eb27c975186ea13a4ac4a86cb137e6191ba
ramu madagala <ramumadagala46@gmail.com>
2025-09-28 01:33:58 +0530

    some message

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi suresh, how are you?"
+echo "suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: what are you learning recently?"
+echo "suresh:: I am learning shell scripting Ramesh"
