5c985082ff7154e4e432797c896ba58642230eea
ramu madagala <ramumadagala46@gmail.com>
2025-09-28 01:54:52 +0530

    some message

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: what are you learning recently?"
+echo "$PERSON2:: I am learning shell scripting $PERSON1"
