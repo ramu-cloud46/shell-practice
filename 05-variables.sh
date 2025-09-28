c82e43324c71f0831b7d2d9d2b037013da20df51
ramu madagala <ramumadagala46@gmail.com>
2025-09-28 20:08:29 +0530

    Add datatypes.sh and other updates

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Please enter your PIN number"
-read  -s PIN_NUMBER # here PIN_NUMBER is variable
+read -s PIN_NUMBER # here PIN_NUMBER is variable
 
 echo "PIN entered is: $PIN_NUMBER"
 
---
+++ 07-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DATE=$(DATE)
+
+START_TIME=$(date +%s)
+
+sleep 10 &
+
+echo "Timestamp executed: $DATE"
