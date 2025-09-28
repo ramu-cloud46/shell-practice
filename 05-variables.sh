3d8c8c2c56d47e698118d3a784f63928724c609d
ramu madagala <ramumadagala46@gmail.com>
2025-09-28 18:29:47 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo"Please enter your PIN number"
-read PIN_NUMBER # here PIN_NUMBER is variable
+echo "Please enter your PIN number"
+read  -s PIN_NUMBER # here PIN_NUMBER is variable
 
 echo "PIN entered is: $PIN_NUMBER"
+
 
---
+++ 08-special-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "who is running this: $USER"
+echo "Home directory of user: $HOME"
